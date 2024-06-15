55827947139bb1479952fbb8a9b6b8c89dfc0d7d
febinth <thfebin@gmail.com>
2024-06-16 00:41:45 +0100

    Added views and templates for fetching investor data

---
+++ preqin_investors_spa/investors/resources/constants.py
@@ -0,0 +1,2 @@
+PREQIN_INVESTORS_API_URL="http://preqin-api:8000/api/investors"
+STATUS_CODE_200=200
--- preqin_investors_spa/investors/views.py
+++ preqin_investors_spa/investors/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from resources.constants import PREQIN_INVESTORS_API_URL, STATUS_CODE_200
+import requests
 
 # Create your views here.
+
+def fetch_investor_data():
+    url = PREQIN_INVESTORS_API_URL
+    response = requests.get(url)
+    if response.status_code == STATUS_CODE_200:
+        return response.json()
+    else:
+        return []
+
+def get_investors(request):
+    investor_data=fetch_investor_data()
+    return render(request, 'investors.html', {'investor_data':investor_data})
+        
