42538494c544e7ecda74f7b5e633e9e901e0d1fe
febinth <thfebin@gmail.com>
2024-06-16 01:05:33 +0100

    Linked views with investor template

--- preqin_investors_spa/investors/resources/constants.py
+++ preqin_investors_spa/investors/resources/constants.py
@@ -1,2 +1,3 @@
-PREQIN_INVESTORS_API_URL="http://preqin-api:8000/api/investors"
+PREQIN_INVESTORS_API_URL="http://localhost:8000/api/investors"
 STATUS_CODE_200=200
+TEMPLATE_INVESTORS="investors.html"
--- preqin_investors_spa/investors/views.py
+++ preqin_investors_spa/investors/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from resources.constants import PREQIN_INVESTORS_API_URL, STATUS_CODE_200
 import requests
+from investors.resources import constants
 
 # Create your views here.
 
 def fetch_investor_data():
-    url = PREQIN_INVESTORS_API_URL
+    url = constants.PREQIN_INVESTORS_API_URL
     response = requests.get(url)
-    if response.status_code == STATUS_CODE_200:
+    if response.status_code == constants.STATUS_CODE_200:
         return response.json()
     else:
         return []
 
 def get_investors(request):
     investor_data=fetch_investor_data()
-    return render(request, 'investors.html', {'investor_data':investor_data})
-        
+    return render(request, constants.TEMPLATE_INVESTORS, {'investor_data':investor_data})     
