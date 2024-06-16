e6301a2dfdd75d0f97aac9ff2548265dbdd15702
febinth <thfebin@gmail.com>
2024-06-16 03:01:39 +0100

    Show commitments for investors

--- preqin_investors_spa/investors/urls.py
+++ preqin_investors_spa/investors/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('investors/', views.get_investors),
-    path('investors/<int:investor_id>/', views.get_investor_details, name='investor_details')
+    path('investors/<int:investor_id>/', views.get_investor_details, name='investor_details'),
+    path('investor/commitment/<str:asset_class>/<int:investor_id>/', views.get_commitments, name='commitment_details')
 ]
